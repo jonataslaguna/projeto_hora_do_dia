88ae9e88b1af51f9ac5d523bc3e3571a4e6870c9
jonataslaguna <jonataslaguna.js@gmail.com>
2023-02-10 09:42:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function carregar(){
+var msg = document.querySelector('div#msg')
+var img = document.querySelector('img#imagem')
+var data = new Date()
+var hora = data.getHours()
+var minutos = data.getMinutes()
+msg.innerHTML = `<p>Agora são ${hora} hora(s) e ${minutos} minutos....</p>`
+    if (hora >= 5 && hora < 12){
+        msg.innerHTML += `Bom Dia!`
+        img.src ='imagens/manha.png'
+        document.body.style.background = '#8acdfa'
+    }else if (hora >= 12 && hora < 18){
+        msg.innerHTML += `Bom Tarde!`
+        img.src ='imagens/tarde.png'
+        document.body.style.background = '#fc731e'
+    } else {
+        msg.innerHTML += `Boa Noite!`
+        img.src ='imagens/noite.png'
+        document.body.style.background = '#081c38'
+    }
+
+}
+
